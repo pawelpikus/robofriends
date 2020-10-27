447d9318aacd251ad590192e72c6801e774004df
Pawel Pikus <pawelpikus7@gmail.com>
2020-10-27 20:57:20 +0100

    add search robots

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CardList from "./CardList";
+import Searchbox from "./Searchbox";
+import { robots } from "./robots";
+import "./App.css";
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1 className="f1">ROBOFRIENDS</h1>
+                <Searchbox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+
+
+}
+
+export default App;
--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({name, username, email, id}) => {
         return (
-        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}/set_set2?size=200x200`} alt="robots"/>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}/set_set3?size=200x200`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{username}</p>
