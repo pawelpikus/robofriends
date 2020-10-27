1b4d3bc77825e4a678472f587e2c4f5327465175
Pawel Pikus <pawelpikus7@gmail.com>
2020-10-27 19:13:22 +0100

    adding CardList Component

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = ({name, username, email, id}) => {
         return (
-        <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}/set_set2?size=200x200`} alt="robots"/>
-            <div className="tc">
+            <div>
                 <h2>{name}</h2>
                 <p>{username}</p>
                 <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
-      <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email}/>
-      <Card id={robots[4].id} name={robots[4].name} username={robots[4].username} email={robots[4].email}/>
-    </div>
+    <CardList robots={ robots } />
   </React.StrictMode>,
   document.getElementById('root')
 );
