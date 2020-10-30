a3b43590730ed336764f7c124b282bae7ac982db
Pawel Pikus <pawelpikus7@gmail.com>
2020-10-30 20:32:56 +0100

    adding styled components

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Card = ({name, username, email, id}) => {
         return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <Main>
             <img src={`https://robohash.org/${id}/set_set1?size=200x200`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{username}</p>
                 <p>{email}</p>
             </div>
-        </div>
+        </Main>
 
     );
 }
 
 export default Card;
+
+//using styled-components attrs to attach class to component, makes it cleaner and reusable
+const Main = styled.main.attrs({
+    className: 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'
+})``
