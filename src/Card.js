7aa9bab23d78efb4aa971c04f28e666b6efbbe1f
Pawel Pikus <pawelpikus7@gmail.com>
2020-10-30 20:13:39 +0100

    adding new branch

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, username, email, id}) => {
         return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}/set_set3?size=200x200`} alt="robots"/>
+            <img src={`https://robohash.org/${id}/set_set1?size=200x200`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{username}</p>
