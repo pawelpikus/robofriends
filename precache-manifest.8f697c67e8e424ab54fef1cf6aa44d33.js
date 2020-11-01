57640949a5d7a92ae23200ef2430304118478b58
Pawel Pikus <pawelpikus7@gmail.com>
2020-11-01 18:47:41 +0100

    Updates

---
+++ precache-manifest.8f697c67e8e424ab54fef1cf6aa44d33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507076d26c096c644af05478ec9551cd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47c3c60fdd3b2225fd1c",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6ad04fbd6099f8a79f83",
+    "url": "/robofriends/static/css/main.13c3f563.chunk.css"
+  },
+  {
+    "revision": "47c3c60fdd3b2225fd1c",
+    "url": "/robofriends/static/js/2.da88648b.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/robofriends/static/js/2.da88648b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad04fbd6099f8a79f83",
+    "url": "/robofriends/static/js/main.c5ebfd84.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/robofriends/static/media/STARWARS.35f31c7a.woff"
+  }
+]);
